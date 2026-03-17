3ebb13235292c38074a179a28920d484a6ba4e40
kebabulon <3dmod07@mail.ru>
2026-03-17 21:32:52 +0300

    feat: create simple ORM models

--- src/kino/models.py
+++ src/kino/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    genres = models.ManyToManyField(Genre)
+
+
+# TODO: create Subscription model
+# class Subscription(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
