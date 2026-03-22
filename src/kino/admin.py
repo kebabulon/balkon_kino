f722a3c4b4a58cb39b895fdbf3db75462115498e
kebabulon <3dmod07@mail.ru>
2026-03-23 00:55:08 +0300

    feat: add MovieAdmin and GenreAdmin

--- src/kino/admin.py
+++ src/kino/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from kino.models import Movie, Genre, Watchlist
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "release_date",
+        "maturity_rating",
+        "runtime",
+        "genre"
+    )
+    filter_horizontal = (
+        'genres',
+    )
+    list_filter = (
+        "maturity_rating",
+        "release_date"
+    )
+    search_fields = (
+        "title",
+        "description"
+    )
+    ordering = ("title",)
+
+    def genre(self, obj):
+        return [genre.name for genre in obj.genres.all()]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+    )
+    search_fields = (
+        "name",
+        "description"
+    )
+    ordering = ("name",)
--- src/kino/models.py
+++ src/kino/models.py
@@ -9,6 +9,9 @@ class Genre(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     MATURITY_RATINGS = {
