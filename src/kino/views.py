bbc3ad356611d28a073592a92c1e321908e0adfa
TheFid12 <zhenya.bytchkov@mail.ru>
2026-03-24 22:34:13 +0300

    feat: added filters

--- src/kino/views.py
+++ src/kino/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework import renderers
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import filters
 
 from kino.permissions import IsAdminUserOrReadOnly
 
@@ -10,9 +11,22 @@ from kino.serializers import MovieSerializer, GenreSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'description']
+    ordering_fields = ['title', 'release_date']
+
+
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        maturity_rating = self.request.query_params.get('maturity_rating')
+        if maturity_rating:
+            queryset = queryset.filter(maturity_rating=maturity_rating)
+        release_date = self.request.query_params.get('release_date')
+        if release_date:
+            queryset = queryset.filter(release_date=release_date)
+        return queryset
 
 
 class GenreViewSet(viewsets.ModelViewSet):
